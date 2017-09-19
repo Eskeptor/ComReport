870e68e9a5d94832f04bf7b4361bf3b0978dc800
Eskeptor <skyvvv624@naver.com>
2017-09-19 16:45:03 +0900

    Add Ex6

--- ImageProcessing/src/javabasic/ex6/Ex6.java
+++ ImageProcessing/src/javabasic/ex6/Ex6.java
@@ -1,4 +1,39 @@
 package javabasic.ex6;
 
+import javax.swing.*;
+import java.awt.*;
+
+class ListFrame extends JFrame {
+    ListFrame(final String _title) {
+        super(_title);
+        final String[] imageNames = {"보글젤", "피콜로", "팬텀", "곰돌이", "드래곤", "스티치", "라이언"};
+        final Icon[] images = {new ImageIcon(getClass().getResource("1.jpg")),
+                new ImageIcon(getClass().getResource("2.jpg")),
+                new ImageIcon(getClass().getResource("3.jpg")),
+                new ImageIcon(getClass().getResource("4.jpg")),
+                new ImageIcon(getClass().getResource("5.jpg")),
+                new ImageIcon(getClass().getResource("6.jpg")),
+                new ImageIcon(getClass().getResource("7.jpg"))};
+        setLayout(new FlowLayout());
+
+        JList<String> list = new JList<>(imageNames);
+        list.setVisibleRowCount(5);
+
+        list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        add(new JScrollPane(list));
+
+        JLabel imageLabel = new JLabel();
+        add(imageLabel);
+
+        list.addListSelectionListener((event)-> imageLabel.setIcon(images[list.getSelectedIndex()]));
+    }
+}
+
 public class Ex6 {
+    public static void main(String[] args) {
+        ListFrame frame = new ListFrame("과제6");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(450, 700);
+        frame.setVisible(true);
+    }
 }
